41641c8c2966dadf3ca47a92272ef44db41f3800
Haja-andri <haja.andri@gmail.com>
2019-05-05 13:17:44 +0200

    Completed the strech challenge

--- assignments/objects.js
+++ assignments/objects.js
@@ -97,7 +97,7 @@ console.log(antonietta.PrintGender());
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
- console.log(kennan.speak());
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 console.log(antonietta.multiplyNums(3,4));
@@ -111,16 +111,44 @@ console.log(antonietta.multiplyNums(3,4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name: 'Susan',
+  age: '70',
+  speak: function(){
+    return 'Hello, my name is ' + this.name;
+  },   
+  child: {
+    name: 'George',
+    age: '50',  
+    speak: function(){
+      return 'Hello, my name is ' + this.name;
+    },   
+      grandChild: {
+      name: 'Sam',
+      age: '30',  
+      speak: function(){
+        return 'Hello, my name is ' + this.name;
+      },   
+        }
+  }
+}
+
 
 // Log the parent object's name
+console.log(parent.name);
 
 // Log the child's age
+console.log(parent.child.age);
 
 // Log the name and age of the grandchild
+console.log(parent.child.grandChild.name);
+console.log(parent.child.grandChild.age);
 
 // Have the parent speak
+console.log(parent.speak());
 
 // Have the child speak
+console.log(parent.child.speak());
 
 // Have the grandchild speak
+console.log(parent.child.grandChild.speak());
